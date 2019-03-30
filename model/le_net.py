ac533cb3685c4c2058276f70044950d0e3767435
Brandon Lee <j474lee@edu.uwaterloo.ca>
2019-03-30 18:02:24 -0400

    reduced model size back to original

--- model/le_net.py
+++ model/le_net.py
@@ -10,8 +10,8 @@ class LeNet(BaseModel):
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 1000)
-        self.fc2 = nn.Linear(1000, num_classes)
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, num_classes)
         self.old_fcs = {}
         self.swap_counter = 0
         self.__set_fc_id__()
@@ -40,7 +40,7 @@ class LeNet(BaseModel):
 
         if not fc_id:
             # new layers
-            self.fc2 = nn.Linear(1000, num_classes)
+            self.fc2 = nn.Linear(50, num_classes)
             fc_id = self.__set_fc_id__()
         else:
             self.__load_fcs__(fc_id)
